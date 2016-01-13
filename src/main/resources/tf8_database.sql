408325b8390ce150c8f611feb7e75ace207a3685
mincong <mincong-h@users.noreply.github.com>
2016-01-13 21:55:42 +0100

    database v2, copiée de bdd ESIGELEC 2016-01-13

--- src/main/resources/tf8_database.sql
+++ src/main/resources/tf8_database.sql
@@ -1,32 +1,28 @@
--- phpMyAdmin SQL Dump 
--- version 4.1.4
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.5.42, for osx10.6 (i386)
 --
--- Client :  127.0.0.1
--- Généré le :  Ven 08 Janvier 2016 à 11:14
--- Version du serveur :  5.6.15-log
--- Version de PHP :  5.5.8
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: 193.52.189.17    Database: ir2016
+-- ------------------------------------------------------
+-- Server version	5.1.73-0ubuntu0.10.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de données :  `tf8_database`
+-- Table structure for table `choisir`
 --
 
--- --------------------------------------------------------
-
---
--- Structure de la table `choisir`
---
-
-CREATE TABLE IF NOT EXISTS `choisir` (
+DROP TABLE IF EXISTS `choisir`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `choisir` (
   `id_personne` int(11) NOT NULL,
   `id_quiz` int(11) NOT NULL,
   `id_proposition` int(11) NOT NULL,
@@ -34,16 +30,30 @@ CREATE TABLE IF NOT EXISTS `choisir` (
   PRIMARY KEY (`id_personne`,`id_quiz`,`id_proposition`),
   KEY `id_personne` (`id_personne`),
   KEY `fk_id_quiz` (`id_quiz`),
-  KEY `fk_id_proposition` (`id_proposition`)
+  KEY `fk_id_proposition` (`id_proposition`),
+  CONSTRAINT `fk_id_proposition` FOREIGN KEY (`id_proposition`) REFERENCES `proposition` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_id_personne` FOREIGN KEY (`id_personne`) REFERENCES `personne` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_id_quiz` FOREIGN KEY (`id_quiz`) REFERENCES `quiz` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Structure de la table `personne`
+-- Dumping data for table `choisir`
 --
 
-CREATE TABLE IF NOT EXISTS `personne` (
+LOCK TABLES `choisir` WRITE;
+/*!40000 ALTER TABLE `choisir` DISABLE KEYS */;
+/*!40000 ALTER TABLE `choisir` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `personne`
+--
+
+DROP TABLE IF EXISTS `personne`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `personne` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(255) NOT NULL,
   `prenom` varchar(255) NOT NULL,
@@ -51,43 +61,79 @@ CREATE TABLE IF NOT EXISTS `personne` (
   `mdp` varchar(255) NOT NULL,
   `droits` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Structure de la table `proposition`
+-- Dumping data for table `personne`
 --
 
-CREATE TABLE IF NOT EXISTS `proposition` (
+LOCK TABLES `personne` WRITE;
+/*!40000 ALTER TABLE `personne` DISABLE KEYS */;
+INSERT INTO `personne` VALUES (1,'kevin','charles','test@mail.com','12345',1000),(2,'12345','12345','mincong.h@gmail.com','12345',0),(3,'','','','',0),(4,'','','','',0),(5,'','','','',0),(6,'','','','',0),(7,'','','','',0),(8,'','','','',0),(9,'','','','',0),(10,'Test','Test','test@test.com','05A671C66AEFEA124CC08B76EA6D30BB',0);
+/*!40000 ALTER TABLE `personne` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `proposition`
+--
+
+DROP TABLE IF EXISTS `proposition`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `proposition` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `libelle` varchar(255) NOT NULL,
   `idQuestion` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `idQuestion` (`idQuestion`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+  KEY `idQuestion` (`idQuestion`),
+  CONSTRAINT `fk_id_question` FOREIGN KEY (`idQuestion`) REFERENCES `question` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Structure de la table `question`
+-- Dumping data for table `proposition`
 --
 
-CREATE TABLE IF NOT EXISTS `question` (
+LOCK TABLES `proposition` WRITE;
+/*!40000 ALTER TABLE `proposition` DISABLE KEYS */;
+/*!40000 ALTER TABLE `proposition` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `question`
+--
+
+DROP TABLE IF EXISTS `question`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `question` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `libelle` varchar(255) NOT NULL,
   `idBonneReponse` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `idBonneReponse` (`idBonneReponse`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+  KEY `idBonneReponse` (`idBonneReponse`),
+  CONSTRAINT `fk_idBonneReponse` FOREIGN KEY (`idBonneReponse`) REFERENCES `proposition` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Structure de la table `quiz`
+-- Dumping data for table `question`
 --
 
-CREATE TABLE IF NOT EXISTS `quiz` (
+LOCK TABLES `question` WRITE;
+/*!40000 ALTER TABLE `question` DISABLE KEYS */;
+/*!40000 ALTER TABLE `question` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `quiz`
+--
+
+DROP TABLE IF EXISTS `quiz`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `quiz` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `libelle` varchar(255) NOT NULL,
   `dateDebutQuiz` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -96,52 +142,52 @@ CREATE TABLE IF NOT EXISTS `quiz` (
   `dateDebutQuestion` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `etape` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Structure de la table `quiz_question`
+-- Dumping data for table `quiz`
 --
 
-CREATE TABLE IF NOT EXISTS `quiz_question` (
+LOCK TABLES `quiz` WRITE;
+/*!40000 ALTER TABLE `quiz` DISABLE KEYS */;
+INSERT INTO `quiz` VALUES (1,'','2016-01-13 15:58:41','2016-01-13 15:58:41',0,'2016-01-13 15:58:41',0);
+/*!40000 ALTER TABLE `quiz` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `quiz_question`
+--
+
+DROP TABLE IF EXISTS `quiz_question`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `quiz_question` (
   `id_quiz` int(11) NOT NULL,
   `id_question` int(11) NOT NULL,
   PRIMARY KEY (`id_quiz`,`id_question`),
-  KEY `fk_idQuestion` (`id_question`)
+  KEY `fk_idQuestion` (`id_question`),
+  CONSTRAINT `fk_idQuestion` FOREIGN KEY (`id_question`) REFERENCES `question` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_idQuiz` FOREIGN KEY (`id_quiz`) REFERENCES `quiz` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Contraintes pour les tables exportées
+-- Dumping data for table `quiz_question`
 --
 
---
--- Contraintes pour la table `choisir`
---
-ALTER TABLE `choisir`
-  ADD CONSTRAINT `fk_id_proposition` FOREIGN KEY (`id_proposition`) REFERENCES `proposition` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_id_personne` FOREIGN KEY (`id_personne`) REFERENCES `personne` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_id_quiz` FOREIGN KEY (`id_quiz`) REFERENCES `quiz` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+LOCK TABLES `quiz_question` WRITE;
+/*!40000 ALTER TABLE `quiz_question` DISABLE KEYS */;
+/*!40000 ALTER TABLE `quiz_question` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Contraintes pour la table `proposition`
---
-ALTER TABLE `proposition`
-  ADD CONSTRAINT `fk_id_question` FOREIGN KEY (`idQuestion`) REFERENCES `question` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Contraintes pour la table `question`
---
-ALTER TABLE `question`
-  ADD CONSTRAINT `fk_idBonneReponse` FOREIGN KEY (`idBonneReponse`) REFERENCES `proposition` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Contraintes pour la table `quiz_question`
---
-ALTER TABLE `quiz_question`
-  ADD CONSTRAINT `fk_idQuestion` FOREIGN KEY (`id_question`) REFERENCES `question` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_idQuiz` FOREIGN KEY (`id_quiz`) REFERENCES `quiz` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-01-13 17:52:46

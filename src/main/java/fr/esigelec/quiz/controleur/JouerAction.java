e687b8d3765108f6586b770d68222e763c9dc1fd
Rodolphe Aguidissou <Rodolphe Aguidissou>
2016-01-13 13:39:14 +0100

    no message

--- src/main/java/fr/esigelec/quiz/controleur/JouerAction.java
+++ src/main/java/fr/esigelec/quiz/controleur/JouerAction.java
@@ -1,11 +1,21 @@
+/**
+ * @author Rodolphe AGUIDISSOU - ESIGELEC 2016
+ */
 package fr.esigelec.quiz.controleur;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+
+import fr.esigelec.quiz.business.ActionService;
+import fr.esigelec.quiz.dao.hibernate.QuizDAOImpl;
+import fr.esigelec.quiz.dto.Personne;
+import fr.esigelec.quiz.dto.Question;
+import fr.esigelec.quiz.dto.Quiz;
 
 
 
@@ -14,10 +24,27 @@ public class JouerAction extends Action {
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
 
-		//TODO : Action Play implementation 
+		//TODO : Action Play implementation 	
+		
+		//UTILS
+		QuizDAOImpl quizdaoimpl = new QuizDAOImpl();
+		HttpSession session = request.getSession();
+		
+		//IN 
+		Personne personne = (Personne) session.getAttribute("personne");
+		int idQuiz = Integer.parseInt(request.getParameter("idQuiz"));
 		
 		
-		return null;
+		//OUT 
+		Quiz quiz = quizdaoimpl.getQuiz(idQuiz);
+		Question question = ActionService.getQuestionByQuizId(idQuiz);
+		
+		
+		session.setAttribute("quiz", quiz);
+		session.setAttribute("question", question);
+		
+		
+		return mapping.findForward("succes");
 		
 	}
 	
--- src/main/java/fr/esigelec/quiz/controleur/VueQuestionAction.java
+++ src/main/java/fr/esigelec/quiz/controleur/VueQuestionAction.java
@@ -1,12 +1,18 @@
+/**
+ * @author Rodolphe AGUIDISSOU - ESIGELEC 2016
+ */
 package fr.esigelec.quiz.controleur;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+
+import fr.esigelec.quiz.dto.Quiz;
 
 public class VueQuestionAction extends Action {
 	
@@ -15,11 +21,18 @@ public class VueQuestionAction extends Action {
 			throws Exception {
 
 		//TODO : Action Question View  implementation 
+		HttpSession session = request.getSession();
+		
+		Quiz quiz = (Quiz) session.getAttribute("quiz");
+		Personne Personne = (Personne) session.getAttribute("personne");
+		int idProposition =  Integer.parseInt((String) session.getAttribute("idPoposition"));
+		hashtablePourcentage	Hashtable<Integer,Float>
+		idBonneReponse	int
+		classement	List<Score>
+
 		
 		
-		
-		
-		
+			
 		return null;
 		
 	}

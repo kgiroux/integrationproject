76ec750d12001474d98d07506cc06dc477ccd46a
wenfeng <elricfeng@gmail.com>
2016-01-18 19:29:11 +0100

    modify VueQuestionQuizAdminAction and struts config to make AjouterQuiz good

--- src/main/java/fr/esigelec/quiz/controleur/VueQuestionQuizAdminAction.java
+++ src/main/java/fr/esigelec/quiz/controleur/VueQuestionQuizAdminAction.java
@@ -5,16 +5,22 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
 
 import fr.esigelec.quiz.dao.IQuestionDAO;
-import fr.esigelec.quiz.dao.IQuizDAO;
 import fr.esigelec.quiz.dao.hibernate.QuestionDAOImpl;
-import fr.esigelec.quiz.dao.hibernate.QuizDAOImpl;
 import fr.esigelec.quiz.dto.Personne;
 
+/**
+ * Get all questions from bdd, then store in attribure for jsp use.
+ * @author C'est pas moi
+ * @modified Wenfeng
+ *
+ */
 public class VueQuestionQuizAdminAction extends Action {
 	
 	private static final Logger vueQuestionQuizAdminAction = Logger.getLogger(VueQuestionQuizAdminAction.class);
@@ -22,21 +28,25 @@ public class VueQuestionQuizAdminAction extends Action {
 	public ActionForward execute(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
+		ActionErrors errors = new ActionErrors();
 
 		try {
 			// Get parameters and sessions
 			Personne p = (Personne) request.getSession().getAttribute("personne");
-			int idQuestion = Integer.parseInt(request.getParameter("idQuestion"));
 			
-			if (p.getDroits() == Personne.ADMIN) {
-				IQuestionDAO questionDAO = new QuestionDAOImpl();
-				request.setAttribute("listeQuestions", questionDAO.listQuestion());
-				IQuizDAO quizDAO = new QuizDAOImpl();
-				request.setAttribute("listeQuiz", quizDAO.listQuiz());
-				/* Need to map to quizAdmin.jsp */
-			} else {
-				vueQuestionQuizAdminAction.info("erreur: droit d'admin requis");
+			if (p == null || p.getDroits() != Personne.ADMIN) {
+				errors.add("err.session.auth", new ActionMessage("err.session.auth.notfound"));
+				if (!errors.isEmpty())
+					addErrors(request, errors);
+				return mapping.findForward("login");
 			}
+			
+			IQuestionDAO questionDAO = new QuestionDAOImpl();
+			request.setAttribute("listeQuestions", questionDAO.listQuestion());
+			
+			/* What's `'listeQuiz' up to ? */
+//			IQuizDAO quizDAO = new QuizDAOImpl();
+//			request.setAttribute("listeQuiz", quizDAO.listQuiz());
 			
 		} catch (Exception e) {
 			e.printStackTrace();

b23a87bef04b4f27dfd5221a5e65163fb379ca3d
Kévin Giroux <giroux.kevin.fr@gmail.com>
2016-01-22 12:37:31 +0100

    Commit

--- Android/QuizIntegration/app/src/main/java/fr/esigelec/gsi/quizintegration/adapter/ListenerQuestion.java
+++ Android/QuizIntegration/app/src/main/java/fr/esigelec/gsi/quizintegration/adapter/ListenerQuestion.java
@@ -1,9 +1,36 @@
 package fr.esigelec.gsi.quizintegration.adapter;
 
+import android.app.usage.UsageEvents;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+
+import fr.esigelec.gsi.quizintegration.Activity.MainActivity;
+import fr.esigelec.gsi.quizintegration.R;
+import fr.esigelec.gsi.quizintegration.utils.AndroidHTTPRequest;
+
 /**
  * Created by kevin on 22/01/2016. Package : fr.esigelec.gsi.quizintegration.adapter Project Name : QuizIntegration
  */
 public class ListenerQuestion
 {
+	public JSONObject OnListener(){
+		JSONObject quizJson = null;
+		do{
+			try
+			{
+				quizJson = new AndroidHTTPRequest ().execute (new String[]{MainActivity.IPSERVER + "AndroidQuizList.do", "GET", null}).get ();
 
+			}catch(Exception ex)
+			{
+				Log.e("ERROR",ex.getMessage());
+			}
+
+
+
+		}while(quizJson.has ("noQuestion") && null != quizJson);
+
+		return quizJson;
+	}
 }

807dbc9837dae279dfb5d50beb8074ba0970fb83
mincong-h <mincong.h@gmail.com>
2016-01-25 07:44:55 +0100

    can use UTF-8 now

---
+++ src/main/java/fr/esigelec/quiz/util/CharacterEncodingFilter.java
@@ -0,0 +1,48 @@
+package fr.esigelec.quiz.util;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Struts 1.3.10 encoding problem
+ * 
+ * @see http://www.coderanch.com/t/557874/Struts/Struts-encoding
+ * @author minconghuang
+ *
+ */
+public class CharacterEncodingFilter implements Filter { 
+	private FilterConfig fc; 
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method
+	}
+	
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req; 
+		HttpServletResponse response = (HttpServletResponse) resp; 
+	
+		request.setCharacterEncoding("UTF8"); 
+		response.setCharacterEncoding("UTF8"); 
+	
+		chain.doFilter(request, response); 
+	
+		request.setCharacterEncoding("UTF8"); 
+		response.setCharacterEncoding("UTF8"); 
+	}
+	
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+}

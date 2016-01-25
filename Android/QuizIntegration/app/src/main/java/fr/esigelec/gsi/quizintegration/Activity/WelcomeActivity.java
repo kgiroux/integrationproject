989dc93e55652bd69a02d681143fa09ec8a79d03
KePace <k.pace.13@groupe-esigelec.fr>
2016-01-25 09:26:06 +0100

    Correction du timer passé à  5 secondes

--- Android/QuizIntegration/app/src/main/java/fr/esigelec/gsi/quizintegration/Activity/WelcomeActivity.java
+++ Android/QuizIntegration/app/src/main/java/fr/esigelec/gsi/quizintegration/Activity/WelcomeActivity.java
@@ -16,7 +16,7 @@ import fr.esigelec.gsi.quizintegration.R;
  */
 public class WelcomeActivity extends AppCompatActivity {
 
-    public static String IPSERVER = "http://10.0.2.2:8080/quiz/";
+    public static String IPSERVER = "http://10.20.35.2:8080/quiz/";
     public static Typeface quizFont;
     public static boolean DEBUG = false;
     public static boolean DEV = false;
